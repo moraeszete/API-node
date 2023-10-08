618c96e8be2615dbab93c41db5e9a03b3288552a
Lucas Moraes <105878736+moraeszete@users.noreply.github.com>
2023-10-08 15:00:47 -0300

    node version add

--- db.js
+++ db.js
@@ -5,5 +5,5 @@ import postgres from 'postgres'
 const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
 const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3D${ENDPOINT_ID}`;
 
-export const sql = postgres(URL, { sql: 'require' });
+export const sql = postgres(URL, { ssl: 'require' });
 
